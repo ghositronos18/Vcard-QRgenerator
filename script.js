fc032349017fbabbcc1c43918711e568566e6171
ghositronos18 <141382556+ghositronos18@users.noreply.github.com>
2025-11-14 14:05:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -132,7 +132,7 @@ function updatePreviewAndQR(data) {
   vcardPreview.innerHTML = `
 
         <div class="card-header">
-          <div class="logo">${company || 'COMPANY'}</div>
+          <div class="logo">${customLogo || 'COMPANY'}</div>
         </div>
         
         
@@ -237,5 +237,6 @@ resetBtn.onclick = () => {
   location.reload();
 
 }
+
 
 
